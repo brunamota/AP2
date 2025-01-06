7170ede4fcc1cc70f0a7e5d8bdfcee649e1ad455
Bruna Mota Barbosa <66503956+brunamota@users.noreply.github.com>
2025-01-06 09:02:16 -0300

    Create Questao1.c

---
+++ Listas/ListaRevisao/Questao1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void calcularQuadrado(int *num) {
+    *num = (*num) * (*num);
+}
+
+int main() {
+    int numero;
+    printf("Digite um número: ");
+    scanf("%d", &numero);
+    calcularQuadrado(&numero);
+    printf("O quadrado é: %d\n", numero);
+    
+}
