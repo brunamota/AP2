5296763d3361e4ff17dd13c9b8b1a7fdc1cbaa11
Bruna Mota Barbosa <66503956+brunamota@users.noreply.github.com>
2025-01-06 09:04:26 -0300

    Create Questao2.c

---
+++ Listas/ListaRevisao/Questao2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    FILE *entrada = fopen("nomes.txt", "r");
+    FILE *saida = fopen("nomes_copiados.txt", "w");
+
+    if (entrada == NULL || saída == NULL) {
+        printf("Erro ao abrir os arquivos!\n");
+    } else{
+        char nome[100];
+        while (fgets(nome, sizeof(nome), entrada) != NULL) {
+        fprintf(saida, "%s", nome);
+      }
+      
+    }
+
+    fclose(entrada);
+    fclose(saida);
+}
